a999517e842ee06f253080c9d4b0127389eda784
Edgard Levy <edgardlevy@gmail.com>
2021-04-10 22:47:54 -0300

    initial commit

---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+
+    td {
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      /* aplica apenas ao primeiro elemento */
+      &:first-child {
+        color: var(--text-title);
+      }
+      &.deposit {
+        color: var(--green);
+      }
+      &.withdraw {
+        color: var(--red);
+      }
+
+    }
+  }
+
+`
